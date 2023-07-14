617dc29670f41d6ba7b6d5356dc60128add22a8e
RartemR13 <RartemR13@yandex.ru>
2023-07-14 23:12:12 +0300

    Create Base Classes

--- src/main/scala/square_equation.scala
+++ src/main/scala/square_equation.scala
@@ -1,2 +1,58 @@
-@main def SquareEquation() =
-    println("Hello!")
+import scala.io.StdIn
+
+object Greeter:
+    def AskCoeffitients() =
+        println("Please, write coeffitient as Doubles.")
+
+    def AskCoeffitientA() =
+        println("Ok, now write first coeffitient: ")
+
+    def AskCoeffitientB() =
+        println("Yep, next, second: ")
+
+    def AskCoeffitientC() =
+        println("At last, free member: ")
+end Greeter
+
+class SquareEquationCoeffitients:
+    private var _a: Double = 0
+    private var _b: Double = 0
+    private var _c: Double = 0
+
+    def a: Double = _a
+    def b: Double = _b
+    def c: Double = _c
+
+    def a_=(newValue: Double) = 
+        _a = newValue
+
+    def b_=(newValue: Double) =
+        _b = newValue
+
+    def c_=(newValue: Double) =
+        _c = newValue
+
+    override def toString(): String = 
+        _a.toString() + " " + _b.toString() + " " + _c.toString()
+end SquareEquationCoeffitients
+
+
+val GetSquareEquationCoeffitient = () =>
+    var coeffitients = SquareEquationCoeffitients()
+    Greeter.AskCoeffitients()
+
+    Greeter.AskCoeffitientA()
+    coeffitients.a = StdIn.readDouble()
+    
+    Greeter.AskCoeffitientB()
+    coeffitients.b = StdIn.readDouble()
+
+    Greeter.AskCoeffitientC()
+    coeffitients.c = StdIn.readDouble()
+    
+    coeffitients
+end GetSquareEquationCoeffitient
+
+@main def main() =
+    println(GetSquareEquationCoeffitient())
+    
